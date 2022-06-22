a93a10e3424b744810a137c22bb5556db50f954d
ahgamut <41098605+ahgamut@users.noreply.github.com>
2022-06-23 01:55:15 +0530

    remove commented code

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,3 @@
-/*
-#![no_main]
-#![no_std]
-#![feature(rustc_private)]
-
-extern crate libc;
-
-#[no_mangle]
-pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
-    // Since we are passing a C string the final null character is mandatory
-    const HELLO: &'static str = "Hello, world! %d + %d = %d\n\0";
-    let x: i32 = 1;
-    let y: i32 = 2;
-    unsafe {
-        libc::printf(HELLO.as_ptr() as *const _, x, y, x+y);
-    }
-    0
-}
-
-#[panic_handler]
-fn my_panic(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
-*/
-
-#![feature(restricted_std)]
-
 pub fn main() {
     println!("Hello World! This is an APE built with Rust.");
 }
